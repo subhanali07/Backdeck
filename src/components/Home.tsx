257ca67aa1d4fa2e4b98f8be2d462fbe21fcd67e
subhanali07 <subhanali200823@gmail.com>
2026-08-07 02:27:40 +0500

    started working on home page and made sum changes too navbar fixed its padding and margin issues

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,20 @@
-
-export default function Home(){
-  
- 
+export default function Home() {
   return (
     <>
-    <section className="mx-18 border-2 h-screen border-black/30">H</section>
-     </>
-  )
+      <div className="border-t-2 border-black/10 mt-20"></div>
+
+      <section className="mx-13 border-x-2 h-screen border-black/10 " id="home">
+        <div className="flex p-7 ">
+          <div className="justify-start">
+            <h1 className="text-5xl mt-2 font-baskerville font-extrabold">
+              Your competitors are still sketching. We already{" "}
+              <span className="text-[#FF2E91]/70">shipped</span>.
+            </h1>
+            <p className="font-mono m-2 mt-4">we've been on deadline(always). and we have always delivered. working with AI Startups to early stage YC and Venture Capital has been our go too. </p>
+          </div>
+          <video src="" about="avideo ig" className="h-100 w-180 ml-20 bg-black/10 rounded-[9px]" />
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Sa from "../assets/Sa.svg";
 
 const Navbar = () => {
   return (
-    <nav className="w-full border-b-black/30 h-auto flex justify-between items-center px-8 py-5">
+    <nav className="w-full border-b-black/30 h-auto z-50 fixed top-0 left-0 flex justify-between items-center px-8 py-5">
       <div className="bg-[#bdbdbd] p-1 rounded-[8px]">
         <div className="flex bg-white rounded-[8px] gap-4 px-13 py-1 transition duration-200 hover:cursor-pointer hover:bg-[#dddada]">
           <img src={Sa} className="size-7 pb-[6px]" />
