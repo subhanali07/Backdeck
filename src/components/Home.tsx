18b81bb08bb2d8cada73533faadf21edd0a85918
subhanali07 <subhanali200823@gmail.com>
2026-08-07 23:20:20 +0500

    somewhere in homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import dihmare from "../assets/dihmare.png";
+
 export default function Home() {
   return (
     <>
@@ -6,13 +8,32 @@ export default function Home() {
       <section className="mx-13 border-x-2 h-screen border-black/10 " id="home">
         <div className="flex p-7 ">
           <div className="justify-start">
-            <h1 className="text-5xl mt-2 font-baskerville font-extrabold">
+            <h1 className="text-4xl mt-2 font-baskerville font-extrabold">
               Your competitors are still sketching. We already{" "}
-              <span className="text-[#FF2E91]/70">shipped</span>.
+              <span className="text-[#FF2E91]/70">shipped</span> products.
             </h1>
-            <p className="font-mono m-2 mt-4">we've been on deadline(always). and we have always delivered. working with AI Startups to early stage YC and Venture Capital has been our go too. </p>
+            <p className="font-mono m-2 mt-4">
+              we've been on deadline(always). and we have always delivered.
+              working with AI Startups to early stage YC and Venture Capital has
+              been our goto.{" "}
+            </p>
+            <div className="mt-7 flex gap-4  items-center">
+              <div className="font-baskerville justify-center items-center flex gap-2  bg-[linear-gradient(to_right,_theme(colors.gray.600),_theme(colors.gray.800),_theme(colors.gray.700),_theme(colors.gray.800))] w-fit px-3 py-[3.5px] rounded-[7px] text-xs text-white font-extrabold shadow-lg shadow-black/30">
+                <img
+                  src={dihmare}
+                  className="rounded-full size-8 object-cover"
+                  alt="Daniyal"
+                />
+                Book call with Daniyal
+              </div>
+              <div className=" w-fit px-6 py-[12px]  rounded-[7px] text-white text-xs  font-extrabold bg-black/30  font-meow">View Work</div>
+            </div>
           </div>
-          <video src="" about="avideo ig" className="h-100 w-180 ml-20 bg-black/10 rounded-[9px]" />
+          <video
+            src=""
+            about="avideo ig"
+            className="h-100 w-180 ml-20 bg-black/10 rounded-[9px]"
+          />
         </div>
       </section>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import Sa from "../assets/Sa.svg";
 
 const Navbar = () => {
   return (
-    <nav className="w-full border-b-black/30 h-auto z-50 fixed top-0 left-0 flex justify-between items-center px-8 py-5">
-      <div className="bg-[#bdbdbd] p-1 rounded-[8px]">
+    <nav className="w-full border-b-black/30 h-auto z-50 fixed top-0 left-0 px-13 flex justify-between items-center py-5">
+      <div className="bg-[#e7e7e7] p-1 rounded-[8px]">
         <div className="flex bg-white rounded-[8px] gap-4 px-13 py-1 transition duration-200 hover:cursor-pointer hover:bg-[#dddada]">
           <img src={Sa} className="size-7 pb-[6px]" />
           <div className="text-black font-baskerville font-bold text-md">
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="flex items-center p-1 gap-[6px] bg-[#bdbdbd] rounded-[8px]">
+      <div className="flex items-center p-1 gap-[6px] bg-[#e7e7e7] rounded-[8px]">
         <div className="flex bg-white rounded-[8px] px-8 py-1 transition duration-200 hover:cursor-pointer hover:bg-[#dddada]">
           /work
         </div>
